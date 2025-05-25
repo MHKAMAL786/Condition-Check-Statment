97ee67d8a46ea97f59301afbf63dc31f48736bf7
unknown <me.hasnainkamal@gmail.com>
2025-05-25 14:36:07 -0700

    1st commit

---
+++ app.js
@@ -0,0 +1,62 @@
+
+// need to check weather user insert a big value than the 100 or less
+var  display = prompt("need to check weather user insert a big value than the 100 or less")
+var value = prompt("Enter a value");
+if (value > 100){
+    alert("value is greater than 100");
+} 
+else{
+    alert("value is less than 100");
+}
+
+// need to check userfirst value is greater or user second value.
+
+var display = prompt("need to check userfirst value is greater or user second value");
+var x = prompt("enter first value");
+var y = prompt("enter second value"); 
+if(x < y){
+    alert("first value is greater");
+}else{
+    alert("second value is greater");
+}
+
+
+// need to check uservalue is perfect divisible by 2 or not 
+
+var display = prompt("need to check uservalue is perfect divisible by 2 or not"); 
+var num = prompt("enter value");
+if(num % 2 ==0){
+    alert("value is divided by 2");
+}else{
+    ("value is not divided by 2");
+}
+
+// need to check uservalue is perfect divisible by 3 or not 
+
+var display = prompt(" need to check uservalue is perfect divisible by 3 or not ");
+var num = prompt("enter value");
+if(num % 3 ==0){
+    alert("value is divided by 3");
+}else{
+    alert("value is not divided by 3");
+}
+
+// need to check uservalue is perfect divisible by 5 or not 
+
+var display = prompt(" need to check uservalue is perfect divisible by 5 or not ");
+var num = prompt("enter value");
+if(num % 5 ==0){
+    alert("value is divided by 5");
+}else{
+    alert("value is not divided by 5");
+}
+
+
+
+
+
+
+
+
+
+
